2df56db3caf16061e4a1ea55115861c2bb48f178
Tom Mason <tom.mason@st-annes.ox.ac.uk>
2019-10-16 18:24:55 +0100

    Remove import

--- js/scene.js
+++ js/scene.js
@@ -1,4 +1,5 @@
-import * as THREE from './three.module.js';
+//import * as THREE from './three.module.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const clock = new THREE.Clock();
